955ae8327b435400aabd5deff551670272bf42ed
irakeshbind <rakeshbind351998@gmail.com>
2024-07-12 10:39:56 +0530

    add some text in the title

--- reactproject/src/Component/Hero/Hero.jsx
+++ reactproject/src/Component/Hero/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <div className='hero container'>
       <div className="hero-text">
-        <h1>We Ensure better education for a better wworld</h1>
+        <h1>you Ensure better education for a better wworld</h1>
         <p>Our cutting-edge curriculam is designed  to empower stdudents wwith the knowledge ,skills and expriences need  to excel in the dynamic  field of education</p>
         <button className='btn'>Explore more<img src={arrow}alt="" classname="arrows" /></button>
       </div>
