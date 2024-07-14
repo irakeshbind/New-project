58b8bee307e1292c62013162561afe3d1e39c7ba
irakeshbind <rakeshbind351998@gmail.com>
2024-07-14 22:59:13 +0530

    Campus gallery images change

--- reactproject/src/Component/Campus/Campus.jsx
+++ reactproject/src/Component/Campus/Campus.jsx
@@ -3,14 +3,14 @@ import './Campus.css'
 import gallery1 from '../../assets/img38.jpg'
 import gallery2 from '../../assets/img37.jpg'
 import gallery3 from '../../assets/img38.jpg'
-import gallery4 from '../../assets/img39.jpg'
+import gallery from '../../assets/img39.jpg'
 // import white from '../../assets/arrow.png'
 import arrow from '../../assets/arrow.png'
 const Campus = () => {
   return (
     <div className='campus'>
         <div className="gallery">
-            <img src={gallery1} alt="" className='campusimg' />
+            <img src={gallery4} alt="" className='campusimg' />
             <img src={gallery2} alt="" className='campusimg' />
             <img src={gallery3} alt="" className='campusimg' />
             <img src={gallery4} alt="" className='campusimg' />
